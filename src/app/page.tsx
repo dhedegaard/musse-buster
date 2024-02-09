d19418c71422a78436f8d693abfc0c38768075cf
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-02-09 18:59:48 +0100

    seo stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import { Board } from './Board'
 
 export const metadata: Metadata = {
   title: 'Musse buster!',
+  description: 'A bubble buster game.',
+  keywords: ['musse', 'bubble', 'buster', 'game', 'dhedegaard'],
+  robots: { index: true, follow: true },
 }
 
 export default function Page() {
