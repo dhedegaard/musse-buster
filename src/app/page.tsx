300d065f3a16760cc22e0e22920897d56221ddf2
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-02-10 19:20:55 +0100

    initial layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
-import { Metadata } from 'next'
 import '../globals/styles.css'
 import { Board } from './Board'
-
-export const metadata: Metadata = {
-  title: 'Musse buster!',
-  description: 'A bubble buster game.',
-  keywords: ['musse', 'bubble', 'buster', 'game', 'dhedegaard'],
-  robots: { index: true, follow: true },
-  manifest: '/manifest.json',
-  icons: { icon: '/favicon.png', apple: '/favicon.png' },
-}
 
 export default function Page() {
   return <Board />
