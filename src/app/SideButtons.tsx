31a6e872a98b3b14fe910993f542c120b87b5f51
Dennis Hedegaard <dennis@dhedegaard.dk>
2024-12-13 21:45:07 +0100

    removed heroicons and icons in general, to remove a dependency

--- src/app/SideButtons.tsx
+++ src/app/SideButtons.tsx
@@ -1,6 +1,5 @@
 'use client'
 
-import { ArrowPathIcon, PauseIcon, PlayIcon } from '@heroicons/react/16/solid'
 import { memo, useCallback } from 'react'
 import { useShallow } from 'zustand/react/shallow'
 import { useGameStore } from '../stores/game-store'
@@ -26,25 +25,16 @@ export const SideButtons = memo(function SideButtons() {
         type="button"
         onClick={handleClicktogglePause}
         disabled={gameState === 'game-over'}
-        className="btn btn-primary btn-sm flex items-center gap-1 whitespace-nowrap text-white"
+        className="btn btn-primary btn-sm flex items-center gap-1 text-white"
       >
-        {gameState === 'paused' ? (
-          <>
-            <PlayIcon width={16} /> <div>Resume (P)</div>
-          </>
-        ) : (
-          <>
-            <PauseIcon width={16} /> <div>Pause (P)</div>
-          </>
-        )}
+        {gameState === 'paused' ? <div>Resume (P)</div> : <div>Pause (P)</div>}
       </button>
       <button
         type="button"
         onClick={handleClickReset}
-        className="btn btn-success btn-sm flex items-center gap-1 whitespace-nowrap bg-green-700 text-white"
+        className="btn btn-success btn-sm bg-green-700 text-white"
       >
-        <ArrowPathIcon width={16} />
-        <div>New game (N)</div>
+        New game (N)
       </button>
     </div>
   )
