31c5c24ef9d235349cd9cfa45af97d403aba5587
Dennis Hedegaard <dennis@dhedegaard.dk>
2025-03-11 18:35:12 +0100

    cleanup imports in eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
-import js from '@eslint/js'
-import { FlatCompat } from '@eslint/eslintrc'
-import globals from 'globals'
-import eslintPluginUnicorn from 'eslint-plugin-unicorn'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
