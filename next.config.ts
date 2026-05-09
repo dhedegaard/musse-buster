4bf9ff1d39c08adda4e22e8f5293777dc4cf2891
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-09 11:48:26 +0200

    optimize zod imports

--- next.config.ts
+++ next.config.ts
@@ -3,4 +3,8 @@ import type { NextConfig } from 'next'
 export default {
   reactStrictMode: true,
   output: 'export',
+  typedRoutes: true,
+  experimental: {
+    optimizePackageImports: ['zod'],
+  },
 } satisfies NextConfig
