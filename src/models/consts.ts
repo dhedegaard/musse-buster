2241e508429bce01d5640adae867d6328afdbe08
Dennis Hedegaard <dennis@dhedegaard.dk>
2026-05-16 14:31:32 +0200

    useless freeze() on const'ed number

--- src/models/consts.ts
+++ src/models/consts.ts
@@ -1,2 +1,2 @@
-export const BOARD_WIDTH = Object.freeze(10 as const)
-export const BOARD_HEIGHT = Object.freeze(14 as const)
+export const BOARD_WIDTH = 10 as const
+export const BOARD_HEIGHT = 14 as const
